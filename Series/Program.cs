69c15791be08d81e6175b0fed8cbcd00a25ffc8c
Tyler Stambaugh <11870675+tylerstambaugh@users.noreply.github.com>
2022-11-21 18:22:18 -0500

    first attempt

--- Series/Program.cs
+++ Series/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/// <summary>
+/// https://exercism.org/tracks/csharp/exercises/series
+/// </summary>
+
+
+Console.WriteLine(Series.Slices("123423453456", 2));
+Console.ReadLine();
+
+
+public static class Series
+{
+    public static string[] Slices(string numbers, int sliceLength)
+    {
+        string[] returnArray = new string[numbers.Length - (sliceLength - 1)];
+
+        for (int i = 0; i < numbers.Length - (sliceLength - 1); i++)
+        {
+            string slice = $"{numbers.Substring(i, sliceLength)}";
+
+        }
+
+        return returnArray;
+    }
+}
